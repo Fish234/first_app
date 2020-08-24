a373a742b453c21a6c01e75ded070cca4c2658dd
Natalia <hourglassbranch25@gmail.com>
2020-08-23 22:05:49 -0400

    insert terminal photo

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'pages/one'
+  get 'pages/two'
+  root 'pages#one'#Setear la página one como página de inicio 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
